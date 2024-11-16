a36aa35e1c2081359c6ee1a4d15f89f52cac9dc5
userkaushal <kaushamahida@gmail.com>
2024-11-16 17:20:57 +0530

    upload changes app

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
